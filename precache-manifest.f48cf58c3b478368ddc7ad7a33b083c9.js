53c5e1d2df4bbe01c72437ff43154dbb6cdcd59a
LongBody <54003924+LongBody@users.noreply.github.com>
2020-12-21 15:05:20 +0700

    Updates

---
+++ precache-manifest.f48cf58c3b478368ddc7ad7a33b083c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "777df6b342159394dc6f2a2da5f4130d",
+    "url": "/foodx/index.html"
+  },
+  {
+    "revision": "9a51d88b01e2c4059736",
+    "url": "/foodx/static/css/main.09e2484e.chunk.css"
+  },
+  {
+    "revision": "9e43930a93d265c5d10f",
+    "url": "/foodx/static/js/2.385663be.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/foodx/static/js/2.385663be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a51d88b01e2c4059736",
+    "url": "/foodx/static/js/main.d6c10b62.chunk.js"
+  },
+  {
+    "revision": "27dbe9cfeabccedc43b9",
+    "url": "/foodx/static/js/runtime-main.6d645803.js"
+  },
+  {
+    "revision": "93f9c92588a66b37be5b7ec4bc950858",
+    "url": "/foodx/static/media/logo.93f9c925.png"
+  }
+]);
